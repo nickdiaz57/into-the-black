0f7d90f5f1289a9e9ee69b406eac321e17300912
nickd <nickdiaz58@gmail.com>
2021-02-01 10:36:25 -0500

    Initial grid generation in HTML and CSS, tile generation function for map

--- frontend/scripts/components/map.js
+++ frontend/scripts/components/map.js
@@ -2,4 +2,19 @@ class Map {
     constructor() {
         console.log('Map loaded')
     }
+
+    generateTiles(sideLength) {
+        let mapObj = {}
+        // let mapObj = []
+        for(let x = 0; x < sideLength; x++) {
+            let col = []
+            for(let y = 0; y < sideLength; y++) {
+                let tile = new Tile(x,y)
+                col.push(tile)
+            }
+            mapObj[x] = col
+            // mapObj.push(col)
+        }
+        return mapObj
+    }
 }
--- frontend/scripts/components/tile.js
+++ frontend/scripts/components/tile.js
@@ -1,5 +1,9 @@
 class Tile {
-    constructor() {
-        console.log('Tile loaded')
+    constructor(x=0, y=0) {
+        this.revealed = false
+        this.occupied = false
+        this.encounters = []
+        this.x = x
+        this.y = y
     }
 }
