bbaae1d5cd4527daa5c88073dd3c1e6be80b42a0
nickd <nickdiaz58@gmail.com>
2021-02-16 17:53:54 -0500

    Read scenes, create buttons when player arrives on tile

--- frontend/scripts/components/map.js
+++ frontend/scripts/components/map.js
@@ -19,7 +19,7 @@ class Map {
         this.tiles = this.generateTiles()
         this.addEvents()
     }
-
+    //create map, add player to map
     generateTiles(sideLength=30) {
         let tileObj = {}
         for(let y = 0; y < sideLength; y++) {
@@ -48,11 +48,12 @@ class Map {
         this.tiles[0][0].html.innerText = this.player.icon
         return this.player
     }
-    
+    //player movement
     movePlayer(dir) {//change color of encounters to differ from player and unvisited encounters when player leaves
         let prevTile = this.findPlayer()
         prevTile.occupied = false
         prevTile.populateTile()
+        textContainer.innerHTML = ""
         
         if (dir === "left") {
             this.player.moveLeft(this.player.moveRange);
@@ -69,6 +70,7 @@ class Map {
         newTile.occupied = true
         newTile.visited = true
         newTile.html.innerText = this.player.icon
+        if (newTile.event) {this.triggerEvent(newTile.event)}
     }
     
     seeTiles() {
@@ -79,8 +81,7 @@ class Map {
             }
         }
     }
-
-    
+    //spread events across map on game start
     addEvents() {
         this.tiles[29][29].event = Event.BEACON
         const revealed = [this.tiles[29][29], this.tiles[28][29], this.tiles[27][29], this.tiles[29][28], this.tiles[29][27], this.tiles[28][28], this.tiles[27][27], this.tiles[27][28], this.tiles[28][27]]
@@ -105,6 +106,26 @@ class Map {
             tile = this.tiles[(Math.floor(Math.random() * 27)) + 1][(Math.floor(Math.random() * 27)) + 1]
         } while (!!tile.event)
         return tile
+    }
+    //handle events when player lands on tile
+    triggerEvent(event) {
+        let message = document.createElement('p')
+        textContainer.append(message)
+        this.readScene(event.scenes['start'], message)
+    }
+
+    readScene(scene, html) {
+        html.innerText = scene.text
+        for(let b in scene.buttons) {
+            this.createButton(scene.buttons[b], b)
+        }
+    }
+
+    createButton(data, id) {
+        let btn = document.createElement('button')
+        btn.innerText = data.value
+        btn.id = id
+        textContainer.append(btn)
     }
     
     findPlayer = () => this.tiles[this.player.position[1]][this.player.position[0]]
